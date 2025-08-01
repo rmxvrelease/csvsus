ffe7acec40e2fcb17ef692738d38baad02057e07
rafa <rmxvgit@gmail.com>
2025-08-01 13:14:22 -0300

    submodule start

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { execSync } from 'child_process';
 
 async function bootstrap() {
+  try {
+    execSync('git submodule init');
+    execSync('git submodule update');
+  } catch {
+    console.error('Failed to initialize submodules');
+  }
+
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   await app.listen(process.env.PORT ?? 3003);
