74a3f3cf0be8e608889b6eb9ebf50569aa85ec9b
rafa <rmxvgit@gmail.com>
2025-08-01 13:57:23 -0300

    no need to copy susd?

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { execSync } from 'node:child_process';
-import { cpSync } from 'node:fs';
 
 async function bootstrap() {
   try {
     execSync('git clone https://github.com/rmxvrelease/susd.git');
-    cpSync('susd', '../susd');
   } catch {
     console.error('Failed to initialize submodules');
   }
